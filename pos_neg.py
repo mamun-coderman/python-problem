71fc334d3e3487a1f8346bbd51ef47eadaffa629
mamun-coderman <mamun.coderman@gmail.com>
2023-07-13 08:56:14 +0600

    positive_negative

---
+++ pos_neg.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the number: "))
+
+if num >= 0:
+    if num == 0:
+        print("The number is zero")
+    else:
+        print("The number is positive")
+if num < 0:
+    print("The number is negative")
