b719ff401e8b27b676968e80f94fc10ecc1ebe20
mamun-coderman <mamun.coderman@gmail.com>
2023-09-14 05:17:28 +0600

    namta code added

---
+++ naamta.py
@@ -0,0 +1,7 @@
+# input any number and return its naamta.
+num = int(input("Enter the number:"))
+#declare and assign initial variable
+m = 1
+while m<= 10:
+    print(num, "*", m, "=", num*m)
+    m += 1
